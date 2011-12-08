4dac8f694d0b9ed98289b1f27541f0f603213a13
exellent <exellent@proc.ru>
2011-12-08 18:54:27 +0400

    working with TreeWalkerTable.java

--- src/parse/errhandler/TreeWalkerTable.java
+++ src/parse/errhandler/TreeWalkerTable.java
@@ -1,90 +1,171 @@
 package parse.errhandler;
 
 import generation.walkers.TreeWalker;
+import generation.walkers.strategys.BottomUpWalkingStrategy;
+import generation.walkers.strategys.IdParsigStrategy;
+import generation.walkers.strategys.TopDownWalkingStrategy;
+import generation.walkers.walkers.FunctionalImplementedChecker;
+import generation.walkers.walkers.IdConvertor;
+import generation.walkers.walkers.IdNotDefinedChecker;
+import generation.walkers.walkers.IdRedefinedChecker;
+import generation.walkers.walkers.IdTableFiller;
+import generation.walkers.walkers.IdTableMaker;
+import generation.walkers.walkers.PositionEstimater;
+import generation.walkers.walkers.TypeMismatchChecker;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class TreeWalkerTable {
-    private LinkedList<Cell> treeTable = new LinkedList<Cell>();
+    private static LinkedList<Cell> treeTable;
+    private LinkedList<Class> unreachableWalkers;
+    private LinkedList<Cell> table;
 
-    public boolean add(TreeWalker walker, LinkedList<TreeWalker> preprocess, LinkedList<ErrorType> error) {
-	for (Cell cell : treeTable) {
-	    if (cell.getWalker() == walker) {
-		return false;
-	    }
-	}
-	return treeTable.add(new Cell(walker, preprocess, error));
+    {
+	treeTable = new LinkedList<Cell>();
+	unreachableWalkers = new LinkedList<Class>();
+
+	initTable();
     }
 
-    public List<TreeWalker> getProcessesByWalker(TreeWalker walker) {
-	for (Cell cell : treeTable) {
-	    if (cell.getWalker() == walker) {
-		return cell.getPreprocesses();
-	    }
-	}
-	return null;
+    private TreeWalkerTable() {
+
     }
 
-    public List<ErrorType> getErrorsByWalker(TreeWalker walker) {
-	for (Cell cell : treeTable) {
-	    if (cell.getWalker() == walker) {
-		return cell.getErrors();
+    private void initTable() {
+	treeTable.add(new Cell(PositionEstimater.class, new LinkedList<Class>(), new LinkedList<ErrorType>()));
+
+	LinkedList<Class> fic = new LinkedList<Class>();
+	fic.add(PositionEstimater.class);
+	treeTable.add(new Cell(FunctionalImplementedChecker.class, fic, new LinkedList<ErrorType>()));
+
+	LinkedList<Class> tm = new LinkedList<Class>();
+	tm.add(PositionEstimater.class);
+	treeTable.add(new Cell(IdTableMaker.class, tm, new LinkedList<ErrorType>()));
+
+	LinkedList<Class> irc = new LinkedList<Class>();
+	irc.add(PositionEstimater.class);
+	treeTable.add(new Cell(IdRedefinedChecker.class, irc, new LinkedList<ErrorType>()));
+
+	LinkedList<Class> indc = new LinkedList<Class>();
+	indc.add(IdTableMaker.class);
+	treeTable.add(new Cell(IdRedefinedChecker.class, indc, new LinkedList<ErrorType>()));
+
+	LinkedList<Class> itf = new LinkedList<Class>();
+	itf.add(IdNotDefinedChecker.class);
+	treeTable.add(new Cell(IdTableFiller.class, itf, new LinkedList<ErrorType>()));
+
+	LinkedList<Class> id = new LinkedList<Class>();
+	id.add(IdNotDefinedChecker.class);
+	treeTable.add(new Cell(IdConvertor.class, id, new LinkedList<ErrorType>()));
+
+	LinkedList<Class> tmc = new LinkedList<Class>();
+	tmc.add(IdTableFiller.class);
+	tmc.add(IdConvertor.class);
+	LinkedList<ErrorType> tmce = new LinkedList<ErrorType>();
+	tmce.add(ErrorType.IdentifierRedefenition);
+	tmce.add(ErrorType.IdentifierUndefined);
+	treeTable.add(new Cell(TypeMismatchChecker.class, tmc, tmce));
+    }
+
+    public static TreeWalkerTable getInstance(List<TreeWalker> walkers) {
+	table = new TreeWalkerTable();
+	table.treeTable.clear();
+
+	deleteUnusedWalkers(walkers, table);
+	deleteUnreachableWalkers(walkers, table);
+
+	return table;
+    }
+
+    private static void deleteUnusedWalkers(List<TreeWalker> walkers, TreeWalkerTable table) {
+	for (TreeWalker walker : walkers) {
+	    Integer position = indexOf(walker.getClass());
+	    table.add(treeTable.get(position), table);
+	}
+    }
+
+    private void add(Cell cell, TreeWalkerTable table) {
+	table.treeTable.add(cell);
+    }
+
+    private static void deleteUnreachableWalkers(List<TreeWalker> walkers, TreeWalkerTable table) {
+	boolean searchFinished = false;
+	Queue<Class> unreachableWalkersQueue = new LinkedList<Class>();
+	unreachableWalkersQueue.addAll((Collection<? extends Class>) table.unreachableWalkers.clone());
+
+	while (unreachableWalkersQueue.size() > 0) {
+	    Class walker = unreachableWalkersQueue.poll();
+	    LinkedList<Cell> removeList = new LinkedList<Cell>();
+	    for (Cell cell : table.treeTable) {
+		if (cell.getPreprocesses().contains(walker)) {
+		    table.unreachableWalkers.add(walker);
+		    unreachableWalkersQueue.offer(walker);
+		    removeList.add(cell);
+		}
+	    }
+	    for (Cell list : removeList) {
+		table.treeTable.remove(list);
 	    }
 	}
-	return null;
     }
-    
-    public static void main(String args[]){
-	TreeWalkerTable twt = new TreeWalkerTable();
-	LinkedList<TreeWalker> one = new LinkedList<TreeWalker>();
-	one.add(null);
-	LinkedList<ErrorType> two = new LinkedList<ErrorType>();
-	two.add(ErrorType.OperandNotFound);
-	
-	twt.add(null, one , two);
-	
-	System.out.print("end");
+
+    private static int indexOf(Class cl) {
+	for (int i = 0; i < treeTable.size(); i++) {
+	    if (treeTable.get(i).getWalker() == cl) {
+		return i;
+	    }
+	}
+
+	return -1;
+    }
+
+    public static void main(String args[]) {
+	LinkedList<TreeWalker> walkers = new LinkedList<TreeWalker>();
+	walkers.add(new PositionEstimater(new IdParsigStrategy()));
+	walkers.add(new FunctionalImplementedChecker(new BottomUpWalkingStrategy(), null));
+	walkers.add(new TypeMismatchChecker(new IdParsigStrategy(), null));
+
+	TreeWalkerTable table = TreeWalkerTable.getInstance(walkers);
+	System.out.print("g");
     }
 }
 
 class Cell {
-    private TreeWalker walker;
-    private LinkedList<TreeWalker> preprocesses;
+    private Class walker;
+    private LinkedList<Class> preprocesses;
     private LinkedList<ErrorType> errorTypes;
-    private boolean executed; 
 
-    {
-	executed = false;
-    }
-    
-    Cell(TreeWalker walker, LinkedList<TreeWalker> preprocess, LinkedList<ErrorType> errorTypes) {
+    public Cell(Class walker, LinkedList<Class> preprocess, LinkedList<ErrorType> errorTypes) {
 	this.walker = walker;
 	this.preprocesses = preprocess;
 	this.errorTypes = errorTypes;
     }
 
-    public TreeWalker getWalker() {
+    public Class getWalker() {
 	return walker;
     }
 
-    public void setWalker(TreeWalker walker) {
+    public void setWalker(Class walker) {
 	this.walker = walker;
     }
 
-    public List<TreeWalker> getPreprocesses() {
-        return preprocesses;
+    public List<Class> getPreprocesses() {
+	return preprocesses;
     }
 
     public List<ErrorType> getErrors() {
-        return errorTypes;
+	return errorTypes;
     }
 
-    public void setPreprocesses(LinkedList<TreeWalker> preprocesses) {
-        this.preprocesses = preprocesses;
+    public void setPreprocesses(LinkedList<Class> preprocesses) {
+	this.preprocesses = preprocesses;
     }
 
     public void setErrors(LinkedList<ErrorType> errorTypes) {
-        this.errorTypes = errorTypes;
+	this.errorTypes = errorTypes;
     }
 }
